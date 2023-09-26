811f1821b846751df87c4646873d908f60604da8
Codrina Lisaru <94629883+codrinaignat@users.noreply.github.com>
2023-09-26 08:43:55 +0300

    Create Define a structure for a person.cpp

---
+++ Define a structure for a person.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// definirea structurii cerute
+struct persoana{
+char nume[20];
+char prenume[20];
+int varsta;
+};
+int main(){
+persoana P={"Ion","Ion",31};
+P.varsta++;
+cout << P.nume << " " << P.prenume << " " << P.varsta;
+}
