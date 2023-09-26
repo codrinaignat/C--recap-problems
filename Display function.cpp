85e2b3571cc19c0d5fd17622f2f1165f24c5c13d
Codrina Lisaru <94629883+codrinaignat@users.noreply.github.com>
2023-09-26 11:10:49 +0300

    Create Display function.cpp

---
+++ Display function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void Message();
+
+int main(){
+    Message();
+}
+
+void Message (){
+    cout<<"***********"<<endl;
+    cout<<"***Hello***"<<endl;
+    cout<<"***********"<<endl;
+}
